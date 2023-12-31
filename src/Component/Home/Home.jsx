56ed6790bc476207a45a60b57053daa0ed2f24df
Mahbubullah <mahbubullahpathan@gmail.com>
2023-12-31 08:14:46 +0600

    product fetch complete

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,26 +1,109 @@
 import { useQuery } from "@tanstack/react-query";
 import { data } from "autoprefixer";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../products/ProductCard";
 
 const Home = () => {
+
+    const [search, setSearch] = useState("");
+
   const { data = [] } = useQuery({
-    queryKey: ["products"],
+    queryKey: ["products", search ? search : ""],
     queryFn: async () => {
-      const res = await axios.get(`https://dummyjson.com/products`);
-      return res.data;
+      const res = await axios.get(`https://dummyjson.com/products/search?q=${search}`);
+      return res.data.products;
     },
   });
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const value = e.target.search.value;
+    setSearch(value);
+  };
+
   console.log(data.products);
+  const [sort, setSort] = useState("Default");
+  const [defaltData, setDefaultData] = useState(data);
+  console.log(defaltData);
+  const selectDefault = () => {
+    setSort("Default");
+    selectDefault(data)
+    
+
+  };
+
+  const selectLow = () => {
+    setSort("Low to high");
+
+    data.sort((a, b) => {
+      let x = parseInt(a.price);
+      let y = parseInt(b.price);
+
+      return x - y;
+    });
+  };
+
+  const selectHigh = () => {
+    setSort("High to low");
+
+    data.sort((a, b) => {
+      let x = parseInt(a.price);
+      let y = parseInt(b.price);
+
+      return y - x;
+    });
+  };
 
   return (
     <div>
-      <h2>kdhfjodshf</h2>
 
+<div className="max-w-6xl mx-auto flex justify-center items-center">
+        
+        <form onSubmit={handleSearch}>
+          <input
+            id="searchValue"
+            className="bg-[#100f0f] bg-opacity-5 text-sm  py-2 px-3 w-72 md:w-96   my-5 rounded-md"
+            type="text"
+            name="search"
+            placeholder="  Search by Products Name"
+          />
+          <button className="bg-[#0D6EFD] text-white text-sm font-semibold px-4 py-2 rounded">
+            Search
+          </button>
+        </form>
+      </div>
+      <p className="flex max-w-[16rem]  justify-evenly items-center mx-4 bg-[#ffcf00] py-2 px-4 rounded-full my-4">
+          <p className="text-xl">Filter By</p>
+          <div className="dropdown ">
+            <label tabIndex={0} className="btn m-1">
+              {sort}
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-52"
+            >
+              <li onClick={selectDefault}>
+                <a>Default</a>
+              </li>
+              <li onClick={selectLow}>
+                <a>Low to high</a>
+              </li>
+              <li onClick={selectHigh}>
+                <a>High to low</a>
+              </li>
+            </ul>
+          </div>
+        </p>
    <div className="grid grid-cols-3 gap-8">
-   {data.products?.map((data, idx) => (
+   {
+    sort === "Default"
+    ? defaltData.map((data, idx) => (
+        <ProductCard key={idx} data={data} />
+      ))
+      :
+   
+   data?.map((data, idx) => (
         <ProductCard key={idx} data={data} />
       ))}
    </div>
